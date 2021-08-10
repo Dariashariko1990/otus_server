7a110f3c758053c4fdd3cdd9bbefd46d3ffbcb36
Dariashariko1990 <Ganesh0192>
2021-08-10 17:35:14 +0300

    Connection bug fixed. Code updated to pass test. README.md added

--- htttpd.py
+++ htttpd.py
@@ -1,16 +1,33 @@
+import argparse
 import threading
 import socket
 import logging
+import time
 from datetime import datetime
 from pathlib import Path
 import os
+import urllib.parse
 
 from httpcls import HTTPStatus, HTTPRequest, HTTPResponse
 
 BIND_ADDRESS = ('localhost', 8080)
-BACKLOG_CONN = 10
-DOCUMENT_ROOT = "/Users/user/PycharmProjects/otus_server/httptest/dir2"
+BACKLOG_CONN = 1000
+NUMBER_WORKERS = 5
+DOCUMENT_ROOT = "/Users/user/PycharmProjects/otus_server"
 
+HTTP_METHODS_ALLOWED = ['GET', 'HEAD']
+
+CONTENT_TYPES_ALLOWED = {
+    ".html": "text/html",
+    ".js": "application/javascript",
+    ".css": "text/css",
+    ".jpeg": "image/jpeg",
+    ".jpg": "image/jpeg",
+    ".png": "image/png",
+    ".gif": "image/gif",
+    ".swf": "application/x-shockwave-flash",
+    ".txt": "text/plain",
+}
 
 logging.basicConfig(
     filename=None,
@@ -18,11 +35,17 @@ logging.basicConfig(
     format='[%(asctime)s] %(levelname).1s %(message)s'
 )
 
-HTTP_METHODS_ALLOWED = ['GET', 'HEAD']
-
-
-class HTTPException(Exception):
-    pass
+parser = argparse.ArgumentParser(description="Passing number of workers and document root")
+parser.add_argument(
+    "-w",
+    dest="number_workers",
+    help="Number of workers. If not specified use default script settings.",
+)
+parser.add_argument(
+    "-d",
+    dest="document_root",
+    help="Path to document root. If not specified use default script settings.",
+)
 
 
 def parse_request(conn):
@@ -33,12 +56,13 @@ def parse_request(conn):
         """
 
     request_data = bytearray()
+    headers_end = bytearray(b'\r\n\r\n')
 
     while True:
         data = conn.recv(1024)
-        if not data:
-            break
         request_data += data
+        if headers_end == data[-4:] or not data:
+            break
 
     request_line = str(request_data, "iso-8859-1").strip()
 
@@ -50,7 +74,35 @@ def parse_request(conn):
 
     method, target, version = parsed_data[0].split()
 
+    # checks for directory traversal
+    if "../" in target:
+        send_error(conn, HTTPStatus.FORBIDDEN)
+
+    try:
+        target = urllib.parse.unquote(target)
+    except:
+        pass
+
     return HTTPRequest(method=method, target=target)
+
+
+def send_error(conn, status):
+    now = datetime.now().strftime("%a, %d %b %Y %H:%M:%S GMT")
+    body = str(status).encode("utf-8")
+
+    headers = (
+        f"HTTP/1.1 {status}",
+        f"Date: {now}",
+        f"Content-Type: text/plain",
+        f"Content-Length: {len(body)}",
+        "Server: Otus-Python-HTTP-Server",
+        "Connection: close"
+    )
+
+    raw_response = "\r\n".join(headers).encode("utf-8")
+    raw_response += b"\r\n\r\n" + body
+    conn.sendall(raw_response)
+    conn.close()
 
 
 def send_response(conn, response):
@@ -64,14 +116,14 @@ def send_response(conn, response):
     headers = (
         f"HTTP/1.1 {response.status}",
         f"Date: {now}",
-        "Content-Type: text/html; charset=utf-8",
+        f"Content-Type: {response.content_type}",
         f"Content-Length: {response.content_length}",
         "Server: Otus-Python-HTTP-Server",
-        "Connection: close",
+        "Connection: close"
     )
 
     raw_response = "\r\n".join(headers).encode("utf-8")
-    raw_response += b"\r\n" + response.body
+    raw_response += b"\r\n\r\n" + response.body
 
     logging.info(
         f"Response is ready to be send: {raw_response}."
@@ -100,30 +152,33 @@ def handle_connection(conn, address, document_root):
     target = request.target
 
     if request.method not in HTTP_METHODS_ALLOWED:
-        raise HTTPException(HTTPStatus.METHOD_NOT_ALLOWED)
+        return send_error(conn, HTTPStatus.METHOD_NOT_ALLOWED)
 
     target = target.partition("/")[-1].partition("?")[0]
 
     path = Path(os.path.join(document_root, target))
+    path = Path(str(path).replace("%20", ' '))
 
     if path.is_dir():
         path /= "index.html"
+    elif target.endswith("/"):
+        return send_error(conn, HTTPStatus.NOT_FOUND)
 
     logging.info(
         f"Target path from the request: {path}."
     )
 
     if not path.is_file():
-        raise HTTPException(HTTPStatus.NOT_FOUND)
+        return send_error(conn, HTTPStatus.NOT_FOUND)
 
     stat = path.stat()
-    content_length = stat.st_size
+    content_length = stat.st_size  # count the length of body
     body = b"" if method == "HEAD" else path.read_bytes()
 
     response = HTTPResponse(
         status=HTTPStatus.OK,
         body=body,
-        content_type=path.suffix,
+        content_type=CONTENT_TYPES_ALLOWED[path.suffix],
         content_length=content_length,
     )
 
@@ -138,7 +193,27 @@ def handle_connection(conn, address, document_root):
     )
 
 
-def serve_forever():
+def wait_connection(server, id):
+    """Waiting forever and accepting connection when incoming.
+        :param server: listening socket object
+        :param id: thread id
+        :return: None
+        """
+    while True:
+        client_socket, address = server.accept()
+        logging.info(
+            f"Thread-{id} has started to process connection from {address[0]}, {address[1]}."
+        )
+        handle_connection(client_socket, address, DOCUMENT_ROOT)
+
+        logging.info(f"Thread-{id} has been stopped.")
+
+
+def serve_forever(workers_number):
+    """Open a listening TCP socket and creating new pool of threads/workers to wait for a connection .
+        :param workers_number: Number of workers.
+        :return: None
+        """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind(BIND_ADDRESS)
@@ -147,18 +222,25 @@ def serve_forever():
             f"Server is running on http://{BIND_ADDRESS[0]}:{BIND_ADDRESS[1]}/ (Press CTRL+C to quit)"
         )
 
-        while True:
-            client_socket, address = server.accept()
-            client_handler = threading.Thread(
-                target=handle_connection,
-                args=(client_socket, address, DOCUMENT_ROOT)
+        for i in range(1, workers_number + 1):
+            thread = threading.Thread(
+                target=wait_connection, args=(server, i)
             )
-            client_handler.daemon = True
-            logging.info(
-                f"New thread has been started to process connection from {address[0]}, {address[1]}."
-            )
-            client_handler.start()
+            thread.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            logging.info("Server is stopping.")
+            return None
 
 
 if __name__ == "__main__":
-    serve_forever()
+    args = parser.parse_args()
+    if args.document_root:
+        DOCUMENT_ROOT = args.document_root
+    if args.number_workers:
+        NUMBER_WORKERS = int(args.number_workers)
+
+    serve_forever(NUMBER_WORKERS)
